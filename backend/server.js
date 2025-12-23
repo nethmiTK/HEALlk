8ef79da27fcc9c5fce97431da4a2e2e799521686
MalithKeerthisinghe <102132303+MalithKeerthisinghe@users.noreply.github.com>
2025-12-23 10:59:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'http://helalanka.lk',
+    'https://helalanka.lk'
   ],
   credentials: true,
 }));
